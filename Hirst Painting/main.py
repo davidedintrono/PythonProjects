87be832963ac4b341e2817eb00554c9f819dad37
davidedintrono <davide.dintrono@gmail.com>
2023-03-06 21:45:45 +0100

    Hirst Painting project

---
+++ Hirst Painting/main.py
@@ -0,0 +1,36 @@
+import turtle as t
+import random as r
+
+# rgb_colors = []
+# colors = colorgram.extract('image.jpg', 30)
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r, g, b)
+#     rgb_colors.append(new_color)
+#
+# print(rgb_colors)
+colors = [(202, 164, 110), (149, 75, 50), (222, 201, 136), (53, 93, 123), (170, 154, 41), (138, 31, 20),
+          (134, 163, 184), (197, 92, 73), (47, 121, 86), (73, 43, 35), (145, 178, 149), (14, 98, 70), (232, 176, 165),
+          (160, 142, 158), (54, 45, 50), (101, 75, 77), (183, 205, 171), (36, 60, 74), (19, 86, 89), (82, 148, 129),
+          (147, 17, 19), (27, 68, 102), (12, 70, 64), (107, 127, 153), (176, 192, 208), (168, 99, 102)]
+
+tim = t.Turtle()
+tim.penup()
+tim.hideturtle()
+t.colormode(255)
+tim.setheading(225)
+tim.forward(300)
+tim.setheading(0)
+
+distance = 50.00
+for _ in range(8):
+    for _ in range(8):
+        tim.dot(20, r.choice(colors))
+        tim.forward(50)
+    tim.goto(-212.00, -212+distance)
+    distance += 50
+
+screen = t.Screen()
+screen.exitonclick()
